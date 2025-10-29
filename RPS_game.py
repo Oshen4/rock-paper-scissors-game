90b3383b7089667b9ddd697bb4bf800906d69d8f
Oshen4 <ojmaratita@gmail.com>
2025-10-29 09:22:14 -0400

    Update RPS_game.py

--- RPS_game.py
+++ RPS_game.py
@@ -1,9 +1,9 @@
 import random
 
-choices = ["Rock", "Paper", "Scissors"]
+choices = ["rock", "paper", "scissors"]
 
 while True:
-    user_choice = input("Enter: Rock, Paper, or Scissors (or use 'quit' to stop): ").lower()
+    user_choice = input("Enter: rock, paper, or scissors (or use 'quit' to stop): ").lower()
 
     if user_choice == 'quit':
         print("Thanks for Playing!")
@@ -18,11 +18,12 @@ while True:
 
     if user_choice == computer_choice:
         print("It's a Tie!")
-    elif (user_choice == "Rock" and computer_choice == "Scissors" ) or \
-         (user_choice == "Scissors" and computer_choice == "Paper") or \
-         (user_choice == "Paper" and computer_choice == "Rock"):
+    elif (user_choice == "rock" and computer_choice == "scissors" ) or \
+         (user_choice == "scissors" and computer_choice == "paper") or \
+         (user_choice == "paper" and computer_choice == "rock"):
         print("You Win!")
     else:
         print("You Lose!")
+
 
     print()
